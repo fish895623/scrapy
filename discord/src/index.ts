89d377270e942c69bff2ac569eb306a003d64a52
fish895623 <dan990429@gmail.com>
2022-03-11 09:14:38 +0900

    Add config information

--- discord/src/index.ts
+++ discord/src/index.ts
@@ -5,6 +5,7 @@ import { Client, Intents } from "discord.js";
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+
 client.on("ready", (msg: any) => {
   if (client.user != null) {
     console.log(`Logged in as ${client.user.tag}!`);
