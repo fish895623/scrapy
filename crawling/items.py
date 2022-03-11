1cacbd7b66de9ee33bde57416d8c70b82af7ca57
fish895623 <dan990429@gmail.com>
2022-03-11 11:20:31 +0900

    Remove Document

--- crawling/items.py
+++ crawling/items.py
@@ -1,13 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class CrawlingItem(scrapy.Item):
-    # define the fields for your item here like:
     title = scrapy.Field()
     date = scrapy.Field()
     content = scrapy.Field()
