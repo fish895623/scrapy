195d13e653df378a19ee1e5bb72a00566921a5cc
fish895623 <dan990429@gmail.com>
2022-03-14 23:37:11 +0900

    Searching Data using async and await

--- discord/src/index.ts
+++ discord/src/index.ts
@@ -1,16 +1,25 @@
-var { channelID, token } = require("../../config.json").config;
+import { channelID, token } from "./config.json";
 
 import { Client, Intents } from "discord.js";
+import { ContentMongoDB, date } from "./UpdateLog";
 // const schedule = require("node-schedule");
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on("ready", (msg: any) => {
+client.on("ready", async (msg: any) => {
   if (client.user != null) {
     console.log(`Logged in as ${client.user.tag}!`);
     // schedule.scheduleJob("* * * * *", () => { // TODO Call from
+    const Data = new ContentMongoDB();
+    await Data.getRawData(
+      `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    ).then((data: any) => {
+      data.forEach((element: any) => {
+        console.log(element);
+      });
+    });
     msg.channels.cache
       .get(`${channelID[0]}`)
       .send("asdf\n<https://google.com/>");
