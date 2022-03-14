a5230bca948d805563429ea7406fac9d57243050
fish895623 <dan990429@gmail.com>
2022-03-14 10:47:57 +0900

    Configure Log4js

--- discord/src/UpdateLog.ts
+++ discord/src/UpdateLog.ts
@@ -5,22 +5,38 @@ const db = config.mongodb;
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
+import log4js from "log4js";
+const logger = log4js.getLogger();
+log4js.configure({
+  appenders: { cheese: { type: "file", filename: "cheese.log" } },
+  categories: { default: { appenders: ["cheese"], level: "trace" } },
+});
+
 var url = `mongodb://${db.user}:${db.password}@${db.host}:${db.port}`;
 
 var Person = new Schema({ data: Number });
 var modelPerson = mongoose.model("steams", Person);
 
-mongoose.connect(url, async (err: any) => {
-  if (err) console.error(err);
-  var a = await modelPerson.find({ data: 123 });
-  console.log(a);
-  await mongoose.connection.close();
-});
-
 mongoose.connection
   .on("error", (err: any) => {
-    console.warn(err);
+    logger.warn(err);
   })
   .once("open", () => {
-    console.log("DB Connected");
+    logger.debug("DB Connected");
   });
+
+function getContent() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(url, async (err: any) => {
+      if (err) console.error(err);
+      var a = await modelPerson.find({ data: 123 });
+      resolve(a);
+      await mongoose.connection.close();
+    });
+  });
+}
+
+getContent().then((data) => {
+  logger.debug(data);
+  logger.info("Get Data");
+});
