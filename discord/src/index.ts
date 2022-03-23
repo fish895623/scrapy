7cce25971b6121a305d71dbdcf71b4de762ff04a
fish895623 <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>
2022-03-23 13:44:34 +0900

    Setup log4js

--- discord/src/index.ts
+++ discord/src/index.ts
@@ -1,9 +1,17 @@
+import path from "path";
 import { channelID, token } from "./config.json";
 
 import { Client, Intents } from "discord.js";
 import { ContentMongoDB, date } from "./UpdateLog";
 // const schedule = require("node-schedule");
+
+// NOTE Interface
 import { Content } from "./interface/Contents";
+
+import log4js from "log4js";
+log4js.configure(path.join(__dirname, "log4js.json"));
+
+const logger = log4js.getLogger("logTest");
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -12,7 +20,8 @@ const client = new Client({
 client.on("ready", async (msg: any) => {
   if (client.user != null) {
     console.log(`Logged in as ${client.user.tag}!`);
-    // schedule.scheduleJob("* * * * *", () => { // TODO Call from
+    // TODO Call from
+    // schedule.scheduleJob("* * * * *", () => {
     const Data = new ContentMongoDB();
     const dataFromMongo: any = await new Promise((resolve) => {
       Data.getRawData(
@@ -22,8 +31,8 @@ client.on("ready", async (msg: any) => {
       });
     });
     dataFromMongo.forEach((element: Content) => {
-      // TODO set interface content interface
-      // TODO Set Logger
+      // TODO Set interface content interface
+      logger.info("Data Send");
       msg.channels.cache
         .get(`${channelID[0]}`)
         .send(`*${element.title}*\n\n\n${element.name}\n${element.address}`);
@@ -31,4 +40,4 @@ client.on("ready", async (msg: any) => {
   }
 });
 
-client.login(token);
+client.login(token).then((res) => logger.info(res));
