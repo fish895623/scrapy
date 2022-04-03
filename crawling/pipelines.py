7062750e409384dfd59b975c1f01c06057359071
fish895623 <dan990429@gmail.com>
2022-04-03 20:34:37 +0900

    Fix

--- crawling/pipelines.py
+++ crawling/pipelines.py
@@ -9,9 +9,6 @@ dotenv.load_dotenv()
 
 class CrawlingPipeline:
     def __init__(self) -> None:
-<<<<<<< HEAD
-        self.client = MongoClient(host='mongodb://root:example@localhost')
-=======
         self.client = MongoClient(
             host='mongodb://{0}:{1}@{2}:{3}'.format(
                 os.getenv('DB_USER'),
@@ -20,18 +17,13 @@ class CrawlingPipeline:
                 os.getenv('DB_PORT'),
             )
         )
->>>>>>> 86f838eccffd54a098c1379d6ca8de04638f2943
         self.db = self.client.steam
 
     def process_item(self, item, spider):
         self.put_items_in_table(item)
         return item
 
-<<<<<<< HEAD
-    def putitemsintable(self, item):
-=======
     def put_items_in_table(self, item):
->>>>>>> 86f838eccffd54a098c1379d6ca8de04638f2943
         if self.db.steam.find_one({'title': item['title']}) is None:
             self.db.steam.insert_one(
                 {
@@ -39,13 +31,9 @@ class CrawlingPipeline:
                     'address': item['address'],
                     'title': item['title'],
                     'content': item['content'],
-<<<<<<< HEAD
-                    'date': datetime.now().strftime('%Y-%-m-%d'),
-=======
                     'date': '{0}-{1}-{2}'.format(
                         datetime.now().year, datetime.now().month, datetime.now().day
                     ),
->>>>>>> 86f838eccffd54a098c1379d6ca8de04638f2943
                 }
             )
         else:
